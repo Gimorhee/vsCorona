65694e5cbab4af5ed10a8b6640cfe31143caf9ce
Danny Rhee <dongyunrhee@gmail.com>
2020-06-09 22:24:50 -0700

    Initial Setup

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export const Main = () => {
+  return (
+    <Fragment>
+      <div className="mainContainer"></div>
+    </Fragment>
+  );
+};
---
+++ src/components/World.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const World = () => {
+  return <div>World Page</div>;
+};
